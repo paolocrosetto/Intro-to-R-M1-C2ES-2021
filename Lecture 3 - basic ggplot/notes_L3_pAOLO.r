b1f80312a7c79deebee86c40248bf0ada0455bf5
paolocrosetto <paolo.crosetto@gmail.com>
2021-09-22 16:30:16 +0200

    notes L3 end

--- Lecture 3 - basic ggplot/notes_L3_pAOLO.r
+++ Lecture 3 - basic ggplot/notes_L3_pAOLO.r
@@ -117,3 +117,186 @@ babynames %>%
   facet_wrap(~name)
 
 
+### partons à l'éxploration des types de plots!!!
+
+## 1 var, discrete
+## mpg$manufacturer
+
+# geom_bar
+# équivalnet de table()
+mpg %>% 
+  ggplot()+
+  aes(x=manufacturer, fill = drv)+
+  geom_bar()
+
+# ordonner les barres
+mpg %>% 
+  group_by(manufacturer) %>% 
+  mutate(N = n()) %>%
+  ggplot()+
+  aes(x=reorder(manufacturer, N), fill = drv)+
+  geom_bar()
+
+
+## 1 var, continue
+
+## histogram -- densité
+## hwy
+mpg %>% 
+  ggplot()+
+  aes(x=hwy)+
+  geom_histogram()
+
+## densité
+mpg %>% 
+  ggplot()+
+  aes(x = hwy)+
+  geom_density()
+
+## boxplot
+mpg %>% 
+  ggplot()+
+  aes(x = hwy)+
+  geom_boxplot()
+
+## violin plot
+mpg %>% 
+  ggplot()+
+  aes(x = 1, y = hwy)+
+  geom_violin()
+
+
+### 2 variables
+
+## 2 continues
+
+# scatter: geom_point()
+mpg %>% 
+  ggplot(aes(x = hwy, y = cty))+
+  geom_point()+
+  geom_abline(intercept = 0, slope = 1, color = "red", linetype = "dashed")
+
+# et si tous les points sont au me endroit?
+# jitter
+# jitter ajoute du bruit blanc à la position des données
+mpg %>% 
+  ggplot(aes(x = hwy, y = cty))+
+  geom_jitter(width = .3, height = .3)+
+  geom_abline(intercept = 0, slope = 1, color = "red", linetype = "dashed")
+
+
+## 2 var, 1 continue 1 discrète
+# conso en hwy par class
+
+# plot vide
+p <- mpg %>% 
+  ggplot()+
+  aes(x = class, y = hwy, color = class, fill = class)
+
+p + geom_boxplot()
+
+# et si  on voulait inverser les axes? 
+
+p + coord_flip()+ geom_boxplot()
+
+## violin plot
+
+p + geom_violin()
+
+
+## raincloud plot
+
+p + geom_jitter(height = 0, width = .2)
+
+
+### 2 variables, les deux sont discrètes
+
+## class and drv
+
+mpg %>% 
+  ggplot() +
+  aes(x = class, y = drv)+
+  geom_count()
+
+## équivalent à
+table(mpg$class, mpg$drv)
+
+
+### 3 variables
+
+## geom_tile
+## pour chaque drive, pour chaque N de cylindre, 
+## quelle est la consommation moyenne?
+
+mpg %>% 
+  group_by(drv, cyl) %>% 
+  summarise(mean_conso = mean(hwy)) %>% 
+  ggplot()+
+  aes(x = drv, y = cyl, fill = mean_conso)+
+  geom_tile()
+
+#### Exercices
+
+library(nycflights13)
+
+df <- flights
+
+## exo 1
+## barplot des 10 destinations les plus fréquentes à partir de JFK
+# tip: use summarise(N = n())
+
+## getting the data
+data <- df %>% 
+  filter(origin == "JFK") %>% 
+  group_by(dest) %>% 
+  summarise(N = n()) %>%   ## tally()
+  top_n(10) 
+
+# base solution
+data %>% 
+  ggplot() + 
+  aes(dest, N)+
+  geom_col()
+
+# reordering by N
+data %>% 
+  ggplot() + 
+  aes(x = reorder(dest, -N), N)+
+  geom_col()
+
+
+## exo 2
+## distributions des vols sur le mois de janvier
+df %>% 
+  filter(month == 1) %>% 
+  ggplot()+
+  aes(x = day)+
+  geom_density()
+
+## exo 3
+## barplot des 20 destinations les plus desservie, par aéroport
+data <- df %>% 
+  group_by(origin, dest) %>% 
+  summarise(N = n()) %>% 
+  top_n(20)
+
+data %>% 
+  ggplot() + 
+  aes(x = dest, y = N, fill = origin)+
+  geom_col()+
+  facet_wrap(.~origin, scales = "free")+
+  coord_flip()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
