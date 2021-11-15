d9b1c503f908559428c17967666ca61ec346ddf2
lugovalexa <85133446+lugovalexa@users.noreply.github.com>
2021-11-15 17:30:07 +0100

    delete

--- Student Area/Alexandra Lugova/R final project.R
+++
@@ -1,9 +0,0 @@
-ultra_rankings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-26/ultra_rankings.csv')
-
-race <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-26/race.csv')
-
-utr <- merge(x = ultra_rankings, y = race, by = 'race_year_id')
-
-utr[ ,c('time', 'event', 'city', 'start_time', 'race_year_id')] <- list(NULL)
-
-
